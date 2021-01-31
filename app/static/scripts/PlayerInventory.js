0cea8d42274a2e957860be117f8f04fb0dbe0340
esther <esther.y.yang@gmail.com>
2021-01-31 11:51:35 -0800

    Display dev cards and implement playing dev cards

--- app/static/scripts/PlayerInventory.js
+++ app/static/scripts/PlayerInventory.js
@@ -1,91 +1,36 @@
 import React from 'react';
 import axios from 'axios';
-import { Form, Card, Button, InputGroup, FormControl, ListGroup } from 'react-bootstrap';
+import PlayerOwnedResource from './PlayerOwnedResource';
+import PlayerOwnedDevCards from './PlayerOwnedDevCards';
+import { Card } from 'react-bootstrap';
 
 class PlayerInventory extends React.Component {
-    constructor(props) {
-        super(props);
-        this.initState = {
-            Brick: 0,
-            Ore: 0,
-            Sheep: 0,
-            Wheat: 0,
-            Wood: 0
-        };
-        this.spendResourcesClick = this.spendResourcesClick.bind(this);
-        this.resourceSelectChange = this.resourceSelectChange.bind(this);
-        this.selected = {...this.initState};
-    }
-    spendResourcesClick() {
-        const reqHeader = { headers: {'Content-Type': 'application/json'} };
-        const reqData = {
-            player: this.props.playerInventory.name,
-            resources: {...this.selected}
-        };
-        axios.post(`/store/deposit?player=${this.props.username}`, reqData, reqHeader)
-            .then(res => {
-                if (res.status >= 200 && res.status < 300) {
-                    this.selected = {...this.initState};
-                    this.props.refreshState();
-                } else {
-                    console.log("spend resources failed 1");
-                }
-            })
-            .catch( res => {
-                console.log("spend resources failed 2");
-            });
-    }
-
-//    using janky class variable to avoid rerender - cuz react unmount and rerender doesn't work properly and keeps checkbox checked
-    resourceSelectChange(e) {
-        const resourceType = e.target.nextElementSibling.textContent.trim();
-        let tempState = {...this.selected};
-        if (e.target.checked) {
-            tempState[resourceType] = this.selected[resourceType] + 1;
-            this.selected = tempState;
-        } else if (this.selected[resourceType] > 0) {
-            tempState[resourceType] = this.selected[resourceType] - 1;
-            this.selected = tempState;
-        }
-    }
-
     render() {
-        let resourceSpreadRender = [];
-        const resources = this.props.playerInventory ? this.props.playerInventory.resource_cards : {};
-        if (!!resources) {
-            Object.keys(resources).forEach(key =>
-            {
-                let i;
-                for(i = 0; i < resources[key]; i++) {
-                    const uniqueHash = Math.random().toString(36).slice(-6); // so react can properly rerender after unmount
-                    resourceSpreadRender.push(
-                    <ListGroup.Item>
-                        <Form.Check
-                          type='checkbox'
-                          key={key+i+uniqueHash}
-                          id={key+i}
-                          label={key}
-                          onChange={this.resourceSelectChange}
-                        />
-                    </ListGroup.Item>
-                    );
-                }
-            });
+        let playerOwnedResourceCards;
+        let playerOwnedDevCards;
+        if (this.props.playerInventory) {
+            playerOwnedResourceCards = (
+                <PlayerOwnedResource
+                    username={this.props.playerInventory.name}
+                    resourceCards={this.props.playerInventory.resource_cards}
+                    refreshState={this.props.refreshState} />
+            );
+
+            playerOwnedDevCards = (
+                <PlayerOwnedDevCards
+                    username={this.props.playerInventory.name}
+                    playedCards={this.props.playerInventory.played_dev_cards}
+                    unplayedCards={this.props.playerInventory.unplayed_dev_cards}
+                    refreshState={this.props.refreshState} />
+            );
         }
         return (
-<Card>
-  <Card.Header>Player Inventory</Card.Header>
-  <Card.Body>
-    <Card.Title>Resource Cards</Card.Title>
-    <div className="overflow-auto" style={{height: '220px', border: '1px solid lightgrey', 'border-radius': '5px'}}>
-    <ListGroup ref = {c => this.resourceListGroup = c}>
-        {resourceSpreadRender}
-    </ListGroup>
-    </div>
-    <Button className="mt-3" variant="dark" onClick={this.spendResourcesClick}>Spend selected</Button>
-  </Card.Body>
-</Card>
+            <Card>
+                <Card.Header>Player Inventory</Card.Header>
+                    {playerOwnedResourceCards}
+                    {playerOwnedDevCards}
+            </Card>
         );
     }
-    }
+}
 export default PlayerInventory;
--- app/static/scripts/StoreDevCard.js
+++ app/static/scripts/StoreDevCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Card, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 class StoreDevCard extends React.Component {
     constructor(props) {
@@ -13,9 +13,6 @@ class StoreDevCard extends React.Component {
         axios.put(`/store/buyDevCard?player=${this.props.username}`)
             .then(res => {
                 if (res.status >= 200 && res.status < 300) {
-                    if(this.qtyInput) {
-                        this.qtyInput.value = 0;
-                    }
                     this.props.refreshState();
                 } else {
                     console.log("Buy dev card failed 1");
@@ -32,21 +29,7 @@ class StoreDevCard extends React.Component {
                 <Card.Header>Dev Cards ({this.props.resourceStock})</Card.Header>
                 <Card.Img variant="top" src={"/static/images/Dev card.png"}/>
                 <Card.Body>
-                    <InputGroup className="mb-3" size="sm">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="resource-qty">Qty</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                                placeholder="0"
-                                aria-label="resource-draw-qty"
-                                aria-describedby="resource-draw-qty"
-                                ref = {c => this.qtyInput = c}
-                        />
-                        <InputGroup.Append>
-                            <Button variant="outline-secondary" onClick={this.buyDevCard}>Draw</Button>
-                        </InputGroup.Append>
-                    </InputGroup>
-
+                    <Button variant="outline-secondary" onClick={this.buyDevCard}>Draw</Button>
                 </Card.Body>
             </Card>
         );
